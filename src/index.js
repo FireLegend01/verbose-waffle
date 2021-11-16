5ef2829e627572b81cc42223fbb5b060f0f096be
FireLegend01 <B0man629@outlook.com>
2021-11-16 04:24:26 +0000

    commit

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import Paddle from '/src/paddle';
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext('2d');
 
-ctx.clearRect(0, 0, 1500, 730);
-
-ctx.fillStyle = '#f00';
-ctx.fillRect(20, 20, 100, 100);
-
 const GAME_WIDTH = 1500;
 const GAME_HEIGHT = 730;
+
+ctx.clearRect(0, 0, 1500, 730);
+
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 
 paddle.draw(ctx)
+
+function gameLoop() {
+    ctx.clearRect(0, 0, 1500, 730)
+    
+}
